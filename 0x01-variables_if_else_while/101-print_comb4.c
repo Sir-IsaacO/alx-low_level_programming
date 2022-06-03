47883d09ce4f6f493964ef533bd6c58e0f50b549
Sunday Isaac Ozovehe <sundayisaacozovehe@gmail.com>
2022-06-03 05:18:10 -0700

    Print Comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,12 @@
+/*
+ * File Author: Sunday Isaac Ozovehe
+ */
+
 #include <stdio.h>
 
 /**
  * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -29,7 +33,7 @@ int main(void)
 		}
 	}
 
-	 putchar('\n');
+	putchar('\n');
 
-	 return (0);
+	return (0);
 }
