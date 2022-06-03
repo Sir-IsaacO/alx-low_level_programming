b3c3d620ac1f44913cde446b056343a1e1a434ae
Sunday Isaac Ozovehe <sundayisaacozovehe@gmail.com>
2022-06-03 04:21:58 -0700

    Print Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/*
+ * File Author: Sunday Isaac Ozovehe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse order.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
