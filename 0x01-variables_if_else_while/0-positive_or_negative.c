0260d96b9e36eed2419d3ea4124d39c637b2d4e8
Sunday Isaac Ozovehe <sundayisaacozovehe@gmail.com>
2022-06-03 03:32:06 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 /*
- * Author of this file is Sunday Isaac Ozovehe
+ * File Author: Sunday Isaac Ozovehe
  */
 
 #include <stdlib.h>
@@ -14,13 +14,16 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
+	
 	return (0);
 }
