2f7424c415ec21a45b7627b6b637a5ef0dd93e94
Sunday Isaac Ozovehe <sundayisaacozovehe@gmail.com>
2022-06-07 14:05:24 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - afunction that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = '0'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
